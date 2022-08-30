cd5b5f7f5f1149e0b22f4537f4c261482b59d550
Anuj Dagdusaheb Pawar <Anuj.Pawar@kbr.com>
2022-08-31 02:55:58 +0530

    After Authentication Enable

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "apehm01b02896": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
