2ce195d4831604aa97e5ca5dcd750714481db65e
Anuj Dagdusaheb Pawar <Anuj.Pawar@kbr.com>
2022-09-02 21:58:58 +0530

    'new'

--- src/App.js
+++ src/App.js
@@ -1,27 +1,76 @@
-//import React from 'react';
-//import './App.css';
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import { API , Storage} from 'aws-amplify';
+import { withAuthenticator } from '@aws-amplify/ui-react';
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
 
 import logo from './logo.svg';
-import "@aws-amplify/ui-react/styles.css";
-import {
-  withAuthenticator,
-  Button,
-  Heading,
-  Image,
-  View,
-  Card,
-} from "@aws-amplify/ui-react";
 
-function App({ signOut }) {
+import ReactDOM from 'react-dom/client';
+
+
+const initialFormState = { AssetNumber: '', AssetName: '', AssetType:'' }
+
+function App() {
+  const [notes, setNotes] = useState([]);
+  const [formData, setFormData] = useState(initialFormState);
+
+  useEffect(() => {
+    fetchNotes();
+  }, []);
+
+  
+  async function fetchNotes() {
+    try {
+      const response = await fetch('https://caefabv1e0.execute-api.us-east-1.amazonaws.com/dev/assets', {
+        method: 'GET',
+        headers: {
+          accept: 'application/json',
+        },
+      });
+      if (!response.ok) {
+        alert("Response code - " + response.ok);
+      }
+      const result = await response.json();
+      alert("Result-" + result);
+      
+    } catch (err) {
+      alert("Error - " + err);
+    }
+
+
+  }
+
+
   return (
-    <View className="App">
-      <Card>
-        <Image src={logo} className="App-logo" alt="logo" />
-        <Heading level={1}>We now have Auth!</Heading>
-      </Card>
-      <Button onClick={signOut}>Sign Out</Button>
-    </View>
+    <div className="App">    
+       
+        <p id="assetdata"> test </p>
+       
+       <button onClick={apidata => {fetchNotes();}}> Get Asset Data </button>
+
+
+           
+       <Authenticator>
+       {({ signOut, user }) => (
+         <main>  
+            <div>                  
+            {
+              notes.map(note => ( <div key={note.AssetNumber}>   <h1>Hello Anuj {user.username}</h1> <h2>{note.AssetNumber}</h2>  <p>{note.AssetName}</p>   <p>{note.AssetType}</p>  </div> ))       
+            }
+            </div>  
+           <button onClick={signOut}>Sign out</button>
+         </main>
+        )}
+      </Authenticator>
+    </div>
+
   );
+}
+
+function Hello(props) {
+  return <h1>Hello World!</h1>;
 }
 
 export default withAuthenticator(App);
